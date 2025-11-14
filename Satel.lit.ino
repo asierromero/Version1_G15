4fe186d2195aee2c705c6435d71000717db1b059
asierromero <asier.romero@estudiantat.upc.edu>
2025-11-14 14:41:10 +0100

    Satel.lit.ino

--- Satel.lit.ino
+++ Satel.lit.ino
@@ -6,26 +6,79 @@ DHT dht(DHTPIN, DHTTYPE);
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-const int led1 = 13; // LED verd indica enviament
-const int led2 = 12; // LED vermell indica error
-
 bool enviarDatos = true; // controla si enviar dades
 unsigned long lastSendMillis = 0;
 const unsigned long sendInterval = 2000UL; // enviar cada 2 segons
-
-bool esperandoTimeout = true;
-int nextTimeoutHT = millis() + 5000;
+//variables alarmas
+bool esperandoTimeout = true; 
+unsigned long nextTimeoutHT = 0;
 
 void setup() {
   Serial.begin(9600);    // comunicació amb el PC
   mySerial.begin(9600);
   dht.begin();
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
+ 
+  nextTimeoutHT = millis() + 4000;
 
 }
+
+void loop() {
+  // Llegir comandes des del PC
+  while (mySerial.available()) {
+    String cmd = mySerial.readStringUntil('\n');
+    cmd.trim();
+    if (cmd.equalsIgnoreCase("Parar")) {
+      enviarDatos = false;
+
+    } else if (cmd.equalsIgnoreCase("Reanudar")) {
+      enviarDatos = true;
+    }
+  }
+   // Comprobar si se pasó el tiempo sin recibir datos del sensor
+  if (esperandoTimeout && (millis() >= nextTimeoutHT)) {
+    mySerial.println("Fallo");
+    Serial.println("Fallo - Timeout");
+    esperandoTimeout = false;
+  }
+ //
+ if (esperandoTimeout && (millis() >= nextTimeoutHT)){
+    mySerial.println ("Fallo"); //Surt a estació de Terra
+    Serial.println("Fallo - Timeout"); // Per depurar
+    esperandoTimeout = false; //resetea
+   }
+
+  // Enviar dades si toca
+  unsigned long now = millis();
+  if (enviarDatos && (now - lastSendMillis >= sendInterval)) {
+    lastSendMillis = now;
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+
+    //reiniciar TIMEOUT
+    if (!isnan(t) && !isnan(h)) {
+      esperandoTimeout = true;
+      nextTimeoutHT = millis() + 5000; // 5 segons
+      
+
+      Serial.print("T:");
+      Serial.print(t);
+      Serial.print(":H:");
+      Serial.println(h);
+
+      mySerial.print("T:");
+      mySerial.print(t);
+      mySerial.print(":H:");
+      mySerial.println(h);
+
+    
+  }
+
+  }
+}
+
+
 
 void loop() {
   // Llegir comandes des del PC
